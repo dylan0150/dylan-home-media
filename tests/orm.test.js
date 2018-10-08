ee2f67f7bc3ea72db27138793617985645154cc4
linuxdropout <dylan.hanner@netacea.com>
2018-10-08 12:18:05 +0100

    Type parsing and adding columns

--- tests/orm.test.js
+++ tests/orm.test.js
@@ -2,15 +2,24 @@ const sinon = require('sinon')
 const test  = require('tape')
 const ORM   = require(`${__dirname}/../lib/orm`)
 
-test('ORM :: constructor defaults', t => {
-    const default_config = {
-        host: 'localhost',
-        user: 'postgres',
-        password: 'postgres',
-        database: 'postgres',
-        port: '5432'
-    }
+const test_config = {
+    host: 'localhost',
+    user: 'postgres',
+    password: '',
+    database: 'homemedia',
+    port: '5432'
+}
+const default_config = {
+    host: 'localhost',
+    user: 'postgres',
+    password: 'postgres',
+    database: 'postgres',
+    port: '5432'
+}
+const TEST_TABLE_NAME = `TEST_TABLE_${Date.now()}`
+const TEST_TABLE2_NAME = `TEST_TABLE2_${Date.now()}`
 
+test('ORM :: constructor defaults', t => {
     const orm = new ORM()
 
     t.deepEqual(orm.config, default_config)
@@ -27,4 +36,58 @@ test('ORM :: parseStatement', t => {
     t.deepEqual(sql, expected_output)
     t.deepEqual(values, expected_values)
     t.end()
+})
+test('ORM :: connect', async t => {
+    const orm = new ORM(test_config)
+    t.deepEqual(orm.config, test_config)
+
+    let err
+    try {
+        const client = await orm.pool.connect()
+        await client.release()
+        await orm.done()
+    } catch (e) {
+        err = e
+        console.error(e)
+    }
+    t.notOk(err)
+
+    t.end()
+})
+test('ORM :: table -> droptable', async t => {
+    const orm = new ORM(test_config)
+    t.deepEqual(orm.config, test_config)
+
+    orm
+        .table(`${TEST_TABLE_NAME}`, 'public', err => {
+            t.notOk(err)
+        })
+        .droptable(`${TEST_TABLE_NAME}`, async err => {
+            t.notOk(err)
+            t.end()
+
+            try {
+                await orm.query(`SELECT * FROM ${TEST_TABLE_NAME}`)
+            } catch (e) {
+                err = e
+            }
+            t.ok(err)
+
+            orm.done()
+        })
+})
+test('ORM :: column -> dropcolumn', async t => {
+    const orm = new ORM(test_config)
+    t.deepEqual(orm.config, test_config)
+
+    orm
+        .table(`${TEST_TABLE2_NAME}`, 'public', err => {
+            t.notOk(err)
+        })
+            .column('id', 'int', {}, err => {
+                t.notOk(err)
+                
+                orm.done()
+                t.end()
+            })
 })
