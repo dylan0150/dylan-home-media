64c1ca056bd55f7ed937c7ec3e092166fcf2db50
Dylan Hanner <dylan.hanner@yahoo.co.uk>
2018-12-09 13:57:05 +0000

    exec arbitary commands

--- index.js
+++ index.js
@@ -1,14 +1,24 @@
 const PORT = 80
 const express = require('express')
+const { command } = require('./lib/utils')
+const bodyParser = require('body-parser')
+const fs = require('fs')
+const apiKey = fs.readFileSync(`${__dirname}/lib/.keys/api`, 'UTF-8').replace(/\s/g,'')
 
 const app = express()
-app.use(express.static('www'))
+app.use(
+    express.static('www'),
+    bodyParser.urlencoded({ extended: true }),
+    bodyParser.json(),
+)
 
 app.listen(PORT)
 
-app.get('/restart', (req, res) => {
-    res
-        .status(200)
-        .json({ ok: true })
-        .end()
+app.get('/command', async (req, res) => {
+    const { key, sh } = req.query
+    if (key !== apiKey) return res.status(403).end()
+
+    let err, result = await command(sh).catch(e => err = e)
+    if (err) return res.status(400).send(err).end()
+    return res.status(200).send(result).end()
 })
