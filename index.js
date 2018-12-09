27998ddd53dc758ec917d8a56cfa0f4fd02a54d6
Dylan Hanner <dylan.hanner@yahoo.co.uk>
2018-12-09 13:02:10 +0000

    basic endpoint

--- index.js
+++ index.js
@@ -5,3 +5,10 @@ const app = express()
 app.use(express.static('www'))
 
 app.listen(PORT)
+
+app.get('/restart', (req, res) => {
+    res
+        .status(200)
+        .json({ ok: true })
+        .end()
+})
