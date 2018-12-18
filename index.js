1459779c898f61f4940632a83d8185f3c217eabd
linuxdropout <dylan.hanner@yahoo.co.uk>
2018-12-18 21:33:05 +0000

    removed un-needed files

--- index.js
+++ index.js
@@ -1,9 +1,6 @@
 const PORT = 80
 const express = require('express')
-const { command } = require('./lib/utils')
 const bodyParser = require('body-parser')
-const fs = require('fs')
-const apiKey = fs.readFileSync(`${__dirname}/lib/.keys/api`, 'UTF-8').replace(/\s/g,'')
 
 const app = express()
 app.use(
